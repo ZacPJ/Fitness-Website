527d376a11d272dae991ead2bd6dd4723c85296d
ZacPJ <zperkinsjones@gmail.com>
2022-09-20 22:53:12 +0100

    API basics, minor bugfixes, still some errors in <div>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import './App.css';
-import Nav from './pages/components/nav';
+//import './App.css'; //--Gets called for in index.js
+import {useEffect, useState} from "react";
+import {Navbar} from './pages/components/nav.js';
+import { fetchExcercise} from "./comps/api_main.js";
 
 function App() {
-  return (
-    <Nav/>
-  )
+  const [storedExcercise, setExcercise] = useState([]);
+  useEffect(()=>{
+    fetchExcercise(setExcercise);
+  },[])
+//  return (
+    //<Nav/>
+ // )
+ 
 }
 
 export default App;
