f16c21fd17c0b2aa377876e40265d50fa3a520b0
ZacPJ <zperkinsjones@gmail.com>
2022-09-21 10:50:47 +0100

    Api output added to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 
 import './App.css';
+import {fetchExcercise } from './pages/components/api_main';
 import Nav from './pages/components/nav';
+import {useState, useEffect} from 'react'
 
 function App() {
+  const [storedExcercise, setExcercise] = useState([])
+  useEffect(()=>{
+    fetchExcercise(setExcercise)
+  },[])
+  console.log(storedExcercise)
+  
   return (
     <Nav/>
   )
