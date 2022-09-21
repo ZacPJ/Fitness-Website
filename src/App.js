1e39a03e7a4627b1ca6cc0e054a78ccbc9549ced
Nathan Neville <nathanneville8@gmail.com>
2022-09-21 13:56:01 +0100

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 
+import { Route, Routes } from "react-router-dom";
+// import styled from 'styled-components';
 import './App.css';
 import Nav from './pages/components/nav';
+import Home from './pages/Home'
+import Userprofile from './pages/Userprofile';
+import Calorie from './pages/Calorie';
+import Weight from './pages/Weight';
+
+
+
 
 function App() {
   return (
-    <Nav/>
+
+    <div>
+      <Nav />
+      <Routes>
+      <Route exact path="/" element={ <Home /> } />
+      <Route exact path="/Weight" element={ <Weight /> } />
+      <Route exact path="/Calorie" element={ <Calorie /> } />
+      <Route exact path="/Userprofile" element={ <Userprofile /> } />
+      </Routes>
+    </div>
+
+
   )
 }
 
---
+++ src/pages/Weight.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Weight (){
+    return(
+        <div>
+            <h1>Weight page</h1>
+        </div>
+    )
+}
+
+export default Weight;
