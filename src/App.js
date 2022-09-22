75af7395029c86d76f4e3c34e93cce0a967d0783
Hasan Khan <hasanmullerkhan@gmail.com>
2022-09-22 15:07:41 +0100

    nav bar finalised

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import {useState, useEffect} from 'react'
+import { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
 // import styled from 'styled-components';
 import './App.css';
-import {fetchExcercise } from './pages/components/api_main';
+import { fetchExcercise } from './pages/components/api_main';
 import Nav from './pages/components/nav';
 import Home from './pages/Home'
 import Userprofile from './pages/Userprofile';
@@ -13,24 +13,22 @@ import Weight from './pages/Weight';
 
 function App() {
   const [storedExcercise, setExcercise] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     fetchExcercise(setExcercise)
-  },[])
+  }, [])
   console.log(storedExcercise)
-  
+
   return (
 
     <div>
       <Nav />
       <Routes>
-      <Route exact path="/" element={ <Home /> } />
-      <Route exact path="/Weight" element={ <Weight /> } />
-      <Route exact path="/Calorie" element={ <Calorie /> } />
-      <Route exact path="/Userprofile" element={ <Userprofile /> } />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/Weight" element={<Weight />} />
+        <Route exact path="/Calorie" element={<Calorie />} />
+        <Route exact path="/Userprofile" element={<Userprofile />} />
       </Routes>
     </div>
-
-
   )
 }
 
