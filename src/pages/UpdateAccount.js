7ef3b48706a50a04d753b6bb09bfe27de4ec6bba
Hasan Khan <hasanmullerkhan@gmail.com>
2022-09-27 12:07:30 +0100

    up to date changes

--- src/pages/UpdateAccount.js
+++ src/pages/UpdateAccount.js
@@ -5,13 +5,8 @@ import UpdateUserInfo from "./components/UpdateComponent";
 function UpdateAccount (props){
     return(
         <div>
-<<<<<<< HEAD
             <h1 className="title">Update Account</h1>
-=======
-            <h1>Update Account</h1>
-
             <UpdateUserInfo userInfo = {props.usersInfo}/>
->>>>>>> 64617a0eb4d1751d1378261b8f7ca12c4739047b
         </div>
     )
 }
