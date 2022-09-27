46ecd926d36a8d2d09a95b5634585791f456b0e4
Hasan Khan <hasanmullerkhan@gmail.com>
2022-09-27 16:30:18 +0100

    conditional rendering complete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,30 +3,44 @@ import { useState, useEffect } from "react";
 import Login from "./components/Login.js"
 import { Route, Routes, Link } from "react-router-dom";
 import SignUp from './SignUp';
+import './Home.css';
 
 function Home(props) {
 
     const [user, setUser] = useState();
-    useEffect(()=>{
+    useEffect(() => {
         props.setIsNav(false)
     }, [])
     return (
+
         <div>
 
-            <h1 className="title">Home page</h1>
+            <div className="wrapper">
 
-            {!user ?
-                <div>
-                    <Login setter={setUser} />
-
-                    <Link to="/SignUp">Create an account</Link>
-                    <Routes><Route exact path="/SignUp" element={<SignUp />} /></Routes>
-
-                </div> :
-                <div>
-                    successfully signed in with the email {user}, this message can be removed later
+                <div id="nav-white">
+                    <div id="nav-left">
+                        <ul>
+                            <Link className="tesla-font-black" to="/">MI40</Link>
+                        </ul>
+                    </div>
                 </div>
-            }
+
+                <h1 className="title">Home page</h1>
+
+                {!user ?
+                    <div className="form">
+                        <Login setter={setUser} />
+
+                        <Link to="/SignUp">Create an account</Link>
+                        <Routes><Route exact path="/SignUp" element={<SignUp />} /></Routes>
+
+                    </div>
+                    :
+                    <div>
+                        successfully signed in with the email {user}, this message can be removed later
+                    </div>
+                }
+            </div>
 
 
         </div>
