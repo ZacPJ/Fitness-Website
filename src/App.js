0868bdab90f18b9d5fe008645fe95f660e8c868b
Hasan Khan <hasanmullerkhan@gmail.com>
2022-09-27 12:04:16 +0100

    more changes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,28 @@
-
 import { useState, useEffect } from 'react'
 import { Route, Routes } from "react-router-dom";
 // import styled from 'styled-components';
 import './App.css';
 import { fetchExercise } from './pages/components/api_main';
-import Navbar from './pages/components/nav';
+import Nav from './pages/components/nav';
 import Home from './pages/Home'
 import Userprofile from './pages/Userprofile';
 import Calorie from './pages/Calorie';
 import Weight from './pages/Weight';
 import UpdateAccount from './pages/UpdateAccount';
 import SignUp from './pages/SignUp';
-import { findUserInfo } from './pages/utils'
-import { getCookie } from './pages/common/index'
+import {findUserInfo} from './pages/utils'
+import {getCookie} from './pages/common/index'
 
 
 
 
 function App() {
 
-<<<<<<< HEAD
-  const [storedExercise, setExercise] = useState([])
-  useEffect(() => {
-    fetchExercise(setExercise)
-  }, [])
-=======
 const [storedExercise, setExercise] = useState([])
 const [userInfo, setUserInfo] = useState([])
 async function pull_userInfo(data){
   await setUserInfo(data)
-  console.log(`This is data sent fromt the child`)
+  console.log(This is data sent fromt the child)
   console.log(data)
 }
 useEffect(()=>{
@@ -48,27 +41,16 @@ useEffect(()=>{
         setUserInfo(cookieUser);
         console.log("signed in with cookies on app.js");
     }
->>>>>>> 64617a0eb4d1751d1378261b8f7ca12c4739047b
   console.log(storedExercise)
+  console.log(User test ${userInfo.name})
+
+
+
+
 
   return (
 
     <div>
-<<<<<<< HEAD
-      <Navbar />
-      <div className="wrapper" style={{ border: "solid black" }}>
-
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/Weight" element={<Weight exercise={storedExercise} />} />
-          <Route exact path="/Calorie" element={<Calorie />} />
-          <Route exact path="/Userprofile" element={<Userprofile />} />
-          <Route exact path="/UdateAccount" element={<UpdateAccount />} />
-          <Route exact path="/SignUp" element={<SignUp />} />
-        </Routes>
-
-      </div>
-=======
       <Nav />
       <Routes>
       <Route exact path="/" element={ <Home /> } />
@@ -78,9 +60,8 @@ useEffect(()=>{
       <Route exact path="/UdateAccount" element={ <UpdateAccount usersInfo = {userInfo}/> } />
       <Route exact path="/SignUp" element={ <SignUp /> } />
       </Routes>
->>>>>>> 64617a0eb4d1751d1378261b8f7ca12c4739047b
     </div>
-      )
+  )
 }
 
 export default App;
