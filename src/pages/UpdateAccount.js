76f1871f9935a281b6cabaf13fd1e3ee188e17b8
ZacPJ <zperkinsjones@gmail.com>
2022-09-29 15:38:27 +0100

    css finalising

--- src/pages/UpdateAccount.js
+++ src/pages/UpdateAccount.js
@@ -4,9 +4,15 @@ import UpdateUserInfo from "./components/UpdateComponent";
 
 function UpdateAccount(props) {
     return (
-        <div>
-            <h1 className="title">Update Account</h1>
-            <UpdateUserInfo userInfo={props.usersInfo} />
+        <div className="mainFlex">
+            <div className="innerFlex1">
+                <div className="innerFlex">
+                    <h1 className="title">Update Account</h1>
+                </div>
+                <div className="innerFlex">
+                    <UpdateUserInfo userInfo={props.usersInfo} />
+                </div>
+            </div>
         </div>
     )
 }
