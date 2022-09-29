4a099ca1faad957cee8eae256b0865c81331d6f3
ZacPJ <zperkinsjones@gmail.com>
2022-09-29 13:47:51 +0100

    back button on signup page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,14 +3,19 @@ import { useState } from "react";
 
 import SignUp from "./components/SignUp.js"
 import '../pages/SignUp.css';
-
+import { useNavigate } from "react-router-dom"
 
 
 import { Route, Routes, Link } from "react-router-dom";
 import Home from './Home';
 
 function SignUpFunc() {
+    const navigate = useNavigate()
     const [user, setUser] = useState();
+    async function goHome(){
+        await navigate("/")
+        await window.location.reload(false);
+    }
     return (
         <div>
             <h1>Sign Up</h1>
@@ -20,7 +25,7 @@ function SignUpFunc() {
             {!user ?
                 <div>
                     <SignUp setter={setUser} />
-                    <Link to="/">return to home page</Link>
+                    <button onClick={goHome}>return to home page</button>
                 </div> :
                 <div>
                     account created, return to <Link to="/">home page</Link> to log in
